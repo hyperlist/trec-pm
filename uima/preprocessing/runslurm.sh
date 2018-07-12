9bf851d41a39d2b72bec016424d340bb89c490eb
Erik Faessler <khituras@gmail.com>
2018-07-12 09:35:55 +0200

    Minor changes

--- uima/preprocessing/runslurm.sh
+++ uima/preprocessing/runslurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --mem 10g
-#SBATCH --cpus-per-task 1
+#SBATCH --cpus-per-task 1 
 #SBATCH -J trecprocessing
 
 java -jar -Xmx8g -Dlogback.configurationFile=/home/faessler/Coding/git/trec2018/uima/preprocessing/config/logback.xml ~/bin/jcore-pipeline-runner-*.jar pipelinerunner.xml
