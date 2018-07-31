0f57fc85a0b6e4e10071256deca702cee7947abf
khituras <chew@gmx.net>
2018-07-31 14:08:33 +0200

    Accounting for the fact that one document can appear in multiple queries.

--- uima/trec2018-pubmed-indexer/src/main/java/de/julielab/jcore/trec2018/pubmed/Trec2018FieldGenerator.java
+++ uima/trec2018-pubmed-indexer/src/main/java/de/julielab/jcore/trec2018/pubmed/Trec2018FieldGenerator.java
@@ -5,6 +5,7 @@ import de.julielab.jcore.consumer.es.FieldGenerationException;
 import de.julielab.jcore.consumer.es.FieldGenerator;
 import de.julielab.jcore.consumer.es.FilterRegistry;
 import de.julielab.jcore.consumer.es.preanalyzed.Document;
+import de.julielab.jcore.consumer.es.preanalyzed.IFieldValue;
 import de.julielab.jcore.consumer.es.preanalyzed.RawToken;
 import de.julielab.jcore.types.*;
 import de.julielab.jcore.types.Date;
@@ -19,32 +20,33 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
 
 import java.util.*;
+import java.util.List;
 
 public class Trec2018FieldGenerator extends FieldGenerator {
 
 
     private static final Set<String> unwantedGsFields = new HashSet<>(Arrays.asList("unified_id", "title", "abstract"));
 
-        public Trec2018FieldGenerator(FilterRegistry filterRegistry) {
-            super(filterRegistry);
-        }
+    public Trec2018FieldGenerator(FilterRegistry filterRegistry) {
+        super(filterRegistry);
+    }
 
-        @Override
-        public Document addFields(JCas jCas, Document document) throws CASException, FieldGenerationException {
-            addPmid(jCas, document);
-            addTitle(jCas, document);
-            addFullAbstract(jCas, document);
-            addAbstractSections(jCas, document);
-            addLastAbstractSectionOrFullAbstract(jCas, document);
-            addPublicationDate(jCas, document);
-            addMeshTags(jCas, document);
-            addDocumentSource(jCas, document);
-            addGenes(jCas, document);
-            addOrganisms(jCas, document);
-            addDocumentClasses(jCas, document);
-            addGsInfo(jCas, document);
-            return document;
-        }
+    @Override
+    public Document addFields(JCas jCas, Document document) throws CASException, FieldGenerationException {
+        addPmid(jCas, document);
+        addTitle(jCas, document);
+        addFullAbstract(jCas, document);
+        addAbstractSections(jCas, document);
+        addLastAbstractSectionOrFullAbstract(jCas, document);
+        addPublicationDate(jCas, document);
+        addMeshTags(jCas, document);
+        addDocumentSource(jCas, document);
+        addGenes(jCas, document);
+        addOrganisms(jCas, document);
+        addDocumentClasses(jCas, document);
+        addGsInfo(jCas, document);
+        return document;
+    }
 
     private void addOrganisms(JCas jCas, Document document) {
         Collection<Organism> organisms = JCasUtil.select(jCas, Organism.class);
@@ -86,99 +88,105 @@ public class Trec2018FieldGenerator extends FieldGenerator {
 
     private void addGsInfo(JCas jCas, Document document) {
         Trec2018FilterBoard filterBoard = filterRegistry.getFilterBoard(Trec2018FilterBoard.class);
-        Map<String, CSVRecord> gsRecords = filterBoard.gsRecords;
+        Map<String, List<CSVRecord>> gsRecords = filterBoard.gsRecords;
         String docId = JCoReTools.getDocId(jCas);
-        CSVRecord record = gsRecords.get(docId);
-        if (record != null) {
-            Map<String, Integer> gsHeaderMap = filterBoard.gsHeaderMap;
-            for (String header : gsHeaderMap.keySet()) {
-                if (unwantedGsFields.contains(header) || StringUtils.isBlank(header))
-                    continue;
-                String value = record.get(header);
-                if (!StringUtils.isBlank(value))
-                    document.addField(header, value);
+        List<CSVRecord> records = gsRecords.get(docId);
+        if (records != null) {
+            for (CSVRecord record : records) {
+                Map<String, Integer> gsHeaderMap = filterBoard.gsHeaderMap;
+                for (String header : gsHeaderMap.keySet()) {
+                    if (unwantedGsFields.contains(header) || StringUtils.isBlank(header))
+                        continue;
+                    String value = record.get(header);
+                    if (!StringUtils.isBlank(value)) {
+                        ArrayFieldValue fValue = (ArrayFieldValue) document.get(header);
+                        if (fValue == null) {
+                            fValue = new ArrayFieldValue();
+                            document.addField(header, fValue);
+                        }
+                        fValue.add(new RawToken(value));
+                    }
+                }
             }
         }
     }
 
     /**
-         * We do this to reproduce the behavior of the parsing algorithm of Michel's TREC-PM-17 coworker where only the last abstract section was used in structured abstracts.
-         * @param jCas
-         * @param document
-         */
-        private void addLastAbstractSectionOrFullAbstract(JCas jCas, Document document) {
-            Collection<AbstractSection> sections = JCasUtil.select(jCas, AbstractSection.class);
-            if (sections.isEmpty()) {
-                JCasUtil.select(jCas, AbstractText.class).stream().findAny().ifPresent(anno ->  document.addField("abstract_lastsection", anno.getCoveredText()));
+     * We do this to reproduce the behavior of the parsing algorithm of Michel's TREC-PM-17 coworker where only the last abstract section was used in structured abstracts.
+     *
+     * @param jCas
+     * @param document
+     */
+    private void addLastAbstractSectionOrFullAbstract(JCas jCas, Document document) {
+        Collection<AbstractSection> sections = JCasUtil.select(jCas, AbstractSection.class);
+        if (sections.isEmpty()) {
+            JCasUtil.select(jCas, AbstractText.class).stream().findAny().ifPresent(anno -> document.addField("abstract_lastsection", anno.getCoveredText()));
+        } else {
+            Optional<AbstractSection> reduce = sections.stream().reduce((a, b) -> b);
+            if (reduce.isPresent()) {
+                AbstractSection lastSection = reduce.get();
+                AbstractSectionHeading heading = (AbstractSectionHeading) lastSection.getAbstractSectionHeading();
+                String nlmCategory = heading.getNlmCategory();
+                document.addField("lastAbstractSectionNlmCategory", nlmCategory);
+                document.addField("abstract_lastsection", lastSection.getCoveredText());
+            }
+        }
+    }
+
+    private void addAbstractSections(JCas jCas, Document document) {
+        Collection<AbstractSection> sections = JCasUtil.select(jCas, AbstractSection.class);
+        if (!sections.isEmpty()) {
+            for (AbstractSection section : sections) {
+                AbstractSectionHeading heading = (AbstractSectionHeading) section.getAbstractSectionHeading();
+                String nlmCategory = heading.getNlmCategory();
+                document.addField("abstract_" + nlmCategory, section.getCoveredText());
+            }
+        }
+    }
+
+    private void addDocumentSource(JCas jCas, Document document) {
+        document.addField("documentSource", "medline");
+    }
+
+    private void addPublicationDate(JCas jCas, Document document) {
+        Header header = JCasUtil.selectSingle(jCas, Header.class);
+        PubType pubType = header.getPubTypeList(0);
+        Date pubDate = pubType.getPubDate();
+        document.addField("publicationDate", pubDate.getMonth() + " " + pubDate.getYear());
+    }
+
+    private void addPmid(JCas jCas, Document document) {
+        String pmid = JCoReTools.getDocId(jCas);
+        document.addField("pubmedId", pmid);
+    }
+
+    private void addMeshTags(JCas jCas, Document document) {
+        Collection<MeshHeading> meshHeadings = JCasUtil.select(jCas, MeshHeading.class);
+        ArrayFieldValue allHeadings = new ArrayFieldValue();
+        ArrayFieldValue majorTopicHeadings = new ArrayFieldValue();
+        ArrayFieldValue minorTopicHeadings = new ArrayFieldValue();
+        for (MeshHeading heading : meshHeadings) {
+            allHeadings.add(new RawToken(heading.getDescriptorName()));
+            if (heading.getDescriptorNameMajorTopic()) {
+                majorTopicHeadings.add(new RawToken(heading.getDescriptorName()));
             } else {
-                Optional<AbstractSection> reduce = sections.stream().reduce((a, b) -> b);
-                if (reduce.isPresent()) {
-                    AbstractSection lastSection = reduce.get();
-                    AbstractSectionHeading heading = (AbstractSectionHeading) lastSection.getAbstractSectionHeading();
-                    String nlmCategory = heading.getNlmCategory();
-                    document.addField("lastAbstractSectionNlmCategory", nlmCategory);
-                    document.addField("abstract_lastsection", lastSection.getCoveredText());
-                }
+                minorTopicHeadings.add(new RawToken(heading.getDescriptorName()));
             }
         }
+        document.addField("meshTags", allHeadings);
+        document.addField("meshTagsMajor", majorTopicHeadings);
+        document.addField("meshMinor", minorTopicHeadings);
+    }
 
-        private void addAbstractSections(JCas jCas, Document document) {
-            Collection<AbstractSection> sections = JCasUtil.select(jCas, AbstractSection.class);
-            if (!sections.isEmpty()) {
-                for (AbstractSection section : sections) {
-                    AbstractSectionHeading heading = (AbstractSectionHeading) section.getAbstractSectionHeading();
-                    String nlmCategory = heading.getNlmCategory();
-                    document.addField("abstract_" + nlmCategory, section.getCoveredText());
-                }
-            }
-        }
+    private void addFullAbstract(JCas jCas, Document document) {
+        Collection<AbstractText> abstractAnnotation = JCasUtil.select(jCas, AbstractText.class);
+        abstractAnnotation.stream().findFirst().ifPresent(abstractText -> document.addField("abstract", abstractText.getCoveredText()));
+    }
 
-        private void addDocumentSource(JCas jCas, Document document) {
-            document.addField("documentSource", "medline");
-        }
-
-        private void addPublicationDate(JCas jCas, Document document) {
-            Header header = JCasUtil.selectSingle(jCas, Header.class);
-            PubType pubType = header.getPubTypeList(0);
-            Date pubDate = pubType.getPubDate();
-            document.addField("publicationDate", pubDate.getMonth() + " " + pubDate.getYear());
-        }
-
-        private void addPmid(JCas jCas, Document document) {
-            String pmid = JCoReTools.getDocId(jCas);
-            document.addField("pubmedId", pmid);
-        }
-
-        private void addMeshTags(JCas jCas, Document document) {
-            Collection<MeshHeading> meshHeadings = JCasUtil.select(jCas, MeshHeading.class);
-            ArrayFieldValue allHeadings = new ArrayFieldValue();
-            ArrayFieldValue majorTopicHeadings = new ArrayFieldValue();
-            ArrayFieldValue minorTopicHeadings = new ArrayFieldValue();
-            for (MeshHeading heading : meshHeadings) {
-                allHeadings.add(new RawToken(heading.getDescriptorName()));
-                if (heading.getDescriptorNameMajorTopic()) {
-                    majorTopicHeadings.add(new RawToken(heading.getDescriptorName()));
-                } else {
-                    minorTopicHeadings.add(new RawToken(heading.getDescriptorName()));
-                }
-            }
-            document.addField("meshTags", allHeadings);
-            document.addField("meshTagsMajor", majorTopicHeadings);
-            document.addField("meshMinor", minorTopicHeadings);
-        }
-
-        private void addFullAbstract(JCas jCas, Document document) {
-            Collection<AbstractText> abstractAnnotation = JCasUtil.select(jCas, AbstractText.class);
-            abstractAnnotation.stream().findFirst().ifPresent(abstractText -> document.addField("abstract", abstractText.getCoveredText()));
-        }
-
-        private void addTitle(JCas jCas, Document document) {
-            Collection<Title> titles = JCasUtil.select(jCas, Title.class);
-            titles.stream().findFirst().ifPresent(title -> document.addField("title", title.getCoveredText()));
-        }
-
-
-
+    private void addTitle(JCas jCas, Document document) {
+        Collection<Title> titles = JCasUtil.select(jCas, Title.class);
+        titles.stream().findFirst().ifPresent(title -> document.addField("title", title.getCoveredText()));
+    }
 
 
 }
--- uima/trec2018-pubmed-indexer/src/main/java/de/julielab/jcore/trec2018/pubmed/Trec2018FilterBoard.java
+++ uima/trec2018-pubmed-indexer/src/main/java/de/julielab/jcore/trec2018/pubmed/Trec2018FilterBoard.java
@@ -8,18 +8,15 @@ import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Trec2018FilterBoard extends FilterBoard {
 
-    public Map<String, CSVRecord> gsRecords;
+    public Map<String, List<CSVRecord>> gsRecords;
+    public Map<String, Integer> gsHeaderMap;
     @ExternalResource(key = "goldstandard")
     private List<String> gsData;
-    public Map<String, Integer> gsHeaderMap;
 
     @Override
     public void setupFilters() {
@@ -30,13 +27,12 @@ public class Trec2018FilterBoard extends FilterBoard {
             while (it.hasNext()) {
                 CSVRecord record = it.next();
                 String trecDocId = record.get("trec_doc_id");
-                if (gsRecords.containsKey(trecDocId)) {
-                    System.out.println("ID: " + trecDocId);
-                    System.out.println("New: " + record);
-                    System.out.println("Old: " + gsRecords.get(trecDocId));
-                    System.out.println("---");
-                }
-                gsRecords.put(trecDocId, record);
+                gsRecords.compute(trecDocId, (id, list) -> {
+                    List<CSVRecord> l = list;
+                    if (list == null) l = new ArrayList<>();
+                    l.add(record);
+                    return l;
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
