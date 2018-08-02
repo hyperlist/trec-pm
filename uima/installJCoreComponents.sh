fc74e67f4428e027c8f380d5bca683bd65b68989
Erik Faessler <khituras@gmail.com>
2018-08-02 17:08:48 +0200

    minor change

--- uima/installJCoreComponents.sh
+++ uima/installJCoreComponents.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Must be executed in the uima/ subdirectory of the repository!
 for i in \
 jcore-pmclassifier-ae \
 jcore-trecpm-extraabstracts-reader; do
 	python createMetaDescriptors.py -c -i -r manual -v 1.0 -u false $i;
 	mvn clean install -f $i/pom.xml;
 done
+echo "Installing the parent pom in this directory"
+mvn clean install
