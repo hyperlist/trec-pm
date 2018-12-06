a476dd0197746aa1f0a5ebcf089e57a4be0981fd
khituras <chew@gmx.net>
2018-12-06 18:18:06 +0100

    Adding a gradle build process to make the handling of the UIMA components easier and clearer

--- uima/setup-scripts/installJCoreComponents.sh
+++ uima/setup-scripts/installJCoreComponents.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Must be executed in the uima/ subdirectory of the repository!
+# To be executed by gradle in the uima/ subdirectory of the repository!
 for i in \
 jcore-pmclassifier-ae \
 jcore-trecpm-extraabstracts-reader; do
-	python createMetaDescriptors.py -c -i -r manual -v 1.0 -u false $i;
+	python setup-scripts/createMetaDescriptors.py -c -i -r manual -v 1.0 -u false $i;
 	mvn clean install -f $i/pom.xml;
 done
 echo "Installing the parent pom in this directory"
