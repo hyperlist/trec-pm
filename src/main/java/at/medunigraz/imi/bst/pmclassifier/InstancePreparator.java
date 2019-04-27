f52529aa7d0df22110adc5eb5925e4e5a6f8b35c
khituras <chew@gmx.net>
2019-04-27 11:17:32 +0200

    Removed the Doc2VecPipe. Was unused and methodically deprecated anyway.
    There are now approaches available nowadays, especially concerning
    contextualized word embeddings.

--- src/main/java/at/medunigraz/imi/bst/pmclassifier/InstancePreparator.java
+++ src/main/java/at/medunigraz/imi/bst/pmclassifier/InstancePreparator.java
@@ -118,9 +118,7 @@ public class InstancePreparator implements Serializable {
         //pipes.add(new HasNegativeBoostWordPipe());
         //pipes.add(new HasDiseasePipe());
        // pipes.add(new HasKeywordPipe());
-        //pipes.add(new Doc2VecPipe());
         //pipes.add(new HasPubTypePipe());
-        //pipes.add(new TopicModelPipe());
         pipes.add(new Token2FeatureVector());
         return pipes;
     }
