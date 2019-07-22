d031eb52ded54fe60680643899f7a1618442bde5
khituras <chew@gmx.net>
2019-07-22 15:46:35 +0200

    Added a test for DocumentEmbedding features with a dummy feature.

--- src/main/java/de/julielab/ir/ltr/features/features/DocumentEmbeddingFeatures.java
+++ src/main/java/de/julielab/ir/ltr/features/features/DocumentEmbeddingFeatures.java
@@ -1,19 +1,27 @@
 package de.julielab.ir.ltr.features.features;
 
+import cc.mallet.types.Instance;
 import cc.mallet.types.Token;
+import de.julielab.ir.ltr.features.Feature;
 
-public abstract class DocumentEmbeddingFeatures {
-    private String featureName;
+public abstract class DocumentEmbeddingFeatures extends Feature {
 
-    public DocumentEmbeddingFeatures(String featureName) {
-        this.featureName = featureName;
+    public DocumentEmbeddingFeatures(String name) {
+        super(name);
     }
+
+    @Override
+    public void assignFeature(Instance inst) {
+        assignDocumentEmbeddingFeatures(inst);
+    }
+
+    protected abstract void assignDocumentEmbeddingFeatures(Instance inst);
 
     protected abstract double[] getDocumentEmbedding(String documentText);
 
     /**
      * <p>Sets the given embedding vector values as feature values to the token.</p>
-     * <p>The feature name for each embedding dimension is the value of the {@link #featureName} field,
+     * <p>The feature name for each embedding dimension is the value of the {@link #name} field,
      * followed by an underscore and the 0-based dimension number.</p>
      *
      * @param token     The MALLET token to add the embedding as feature values to.
@@ -22,7 +30,7 @@ public abstract class DocumentEmbeddingFeatures {
     protected void addDocumentEmbeddingFeatures(Token token, double[] embedding) {
         for (int i = 0; i < embedding.length; i++) {
             double d = embedding[i];
-            token.setFeatureValue(featureName + "_" + i, d);
+            token.setFeatureValue(name + "_" + i, d);
         }
     }
 }
