c430788b595885c780ed55b4eb86f37b3c4bd100
Michel Oleynik <michelole@gmail.com>
2019-10-09 17:25:37 +0200

    Add support for statistics across several years

--- R/graphs.R
+++ R/graphs.R
@@ -13,35 +13,73 @@ extra_color <- "darkred"
 # tug_colors <- c(hpipubboost=tug_red, hpipubnone=tug_blue, hpipubbase=tug_green, hpipubclass=tug_magenta, hpipubcommon=tug_orange,
 #                     hpictboost=tug_red, hpictphrase=tug_blue, hpictbase=tug_green, hpictall=tug_magenta, hpictcommon=tug_orange)
 
-
-
+# XXX Set metrics
 # Note that official metrics are generated with an older version of trec_eval that uses different names for metrics.
+# TODO normalize all metrics to 9.0 format
 # trec_eval 8.1: "P10", "R-prec", "ircl_prn."
 # trec_eval 9.0+: "P_10", "Rprec", "iprec_at_recall_"
 # metrics <- c("P5", "P10", "P15")
-# metrics <- c("infNDCG", "P10", "R-prec")
-metrics <- c("ndcg", "P_10", "set_recall")
+metrics <- c("infNDCG", "P_10", "Rprec")      # Default metrics
+# metrics <- c("ndcg", "P_10", "set_recall")  # Michel's thesis
 # iprec_at_recall <- "ircl_prn."
 iprec_at_recall <- "iprec_at_recall_"
 
+# XXX Set input folder
+# input_folder <- "docs/2017/final-results/clinical-trials"
+# input_folder <- "docs/2018/final-results/biomedical-articles"
+input_folder <- "stats"
+
+# XXX Set year. Optional when .trec_eval files already contain topics in the format year-topic (e.g. "201712").
+# year <- 2017
+# year <- 2018
+year <- ""
+
+# XXX Set task
+# task_name <- "ct"
+task_name <- "ba"
+
+old_wd = getwd()
+
+### Statistics of all participants runs ###
+collect_stats <- function() {
+  stats_files <- list.files("R", pattern="*.stats")
+  
+  stats <- data.frame()
+  for (stats_file in stats_files) {
+    matches <- str_match(stats_file, "([0-9]{4})-([a-z]{2}).stats")
+    year <- matches[2]
+    task <- matches[3]
+    
+    file_stats <- read.table(paste("R", stats_file, sep="/"), skip=5)
+    names(file_stats) <- c("topic", "infNDCG.best", "infNDCG.median", "infNDCG.worst",
+                           "P_10.best", "P_10.median", "P_10.worst",
+                           "Rprec.best", "Rprec.median", "Rprec.worst")
+    if (year == 2017 && task == "ct") {
+      names(file_stats) <- c("topic", "P_5.best", "P_5.median", "P_5.worst",
+                             "P_10.best", "P_10.median", "P_10.worst",
+                             "P_15.best", "P_15.median", "P_15.worst")
+    }
+    
+    # Add task and year
+    file_stats <- file_stats %>%
+      mutate(task = task) %>%
+      mutate(year = year) %>%
+      mutate(yeartopic = paste0(year, sprintf("%1d", topic))) # FIXME %02d
+    
+    stats <- stats %>% bind_rows(file_stats)
+  }
+  stats$yeartopic <- as.factor(stats$yeartopic)
+  
+  return(stats)
+}
+stats <- collect_stats()
+task_stats <- filter(stats, task == task_name)
+
+### Results ###
+setwd(input_folder)
+
 # Use .trec_eval files to discover run ids, since they're always present (including 2017 CT).
 run_ids <- gsub(".trec_eval", "", list.files(pattern="*.trec_eval"))
-
-
-# Best, median and worst are averages over all topics
-# TODO get always metrics from 2017 and 2018 and merge for plots per topic
-stats_file <- list.files(pattern="*.stats")[0]
-if (file.exists(stats_file)) {
-  stats <- read.table(stats_file, skip=5)
-  metrics_best <- c(colMeans(stats)[2], colMeans(stats)[5], colMeans(stats)[8])
-  metrics_median <- c(colMeans(stats)[3], colMeans(stats)[6], colMeans(stats)[9])
-  metrics_worst <- c(colMeans(stats)[4], colMeans(stats)[7], colMeans(stats)[10])
-}
-
-# Input folder
-# setwd("docs/2017/final-results/clinical-trials")
-# setwd("docs/2018/final-results/biomedical-articles")
-setwd("stats")
 
 collect_results <- function(run_ids) {
   results <- data.frame()
@@ -70,10 +108,15 @@ results <- collect_results(run_ids)
 ### Boxplots ###
 boxplots <- function(results) {
   plots <- list()
-  for (i in metrics) {
+  for (metric in metrics) {
     test <- results %>%
-      filter(measure==i,topic!='all') %>%
+      filter(measure==metric,topic!='all') %>%
       mutate(value = as.numeric(value))
+    
+    # Skip metric if data is not available (e.g. infNDCG for CT 2017)
+    if (nrow(test) == 0) {
+      next
+    }
   
     means <- aggregate(value ~ run, test, mean)
   
@@ -81,7 +124,7 @@ boxplots <- function(results) {
       geom_boxplot(alpha=0.8) +
       # scale_fill_manual(values=tug_colors) +
       xlab("") +
-      ylab(i) +
+      ylab(metric) +
       theme_linedraw() +
       theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
             plot.background = element_blank(),
@@ -91,23 +134,23 @@ boxplots <- function(results) {
       #geom_text(data = means, aes(label = round(value,4), y = value + 0.04)) +
       guides(fill=FALSE) + scale_y_continuous(breaks=seq(0,1,0.1), limits=c(0,1))
   
-    if (exists("stats")) {
-      g <- g + geom_hline(yintercept=metrics_best[i], linetype="dashed", color = extra_color)
-      # + geom_text(aes(0.6,metrics_best[i],label="best", vjust = 1), color=extra_color)
-  
-      g <- g + geom_hline(yintercept=metrics_median[i], linetype="dashed", color = extra_color)
-      # + geom_text(aes(0.7,metrics_median[i],label="median", vjust = 1), color=extra_color)
-  
-      # g <- g + geom_hline(yintercept=metrics_worst[i], linetype="dashed", color = extra_color)
-      # + geom_text(aes(0.7,metrics_worst[i],label="worst", vjust = 1), color=extra_color)
+    best_column <- paste0(metric, ".best")
+    if (best_column %in% colnames(task_stats)) {
+      average_best <- mean(task_stats[best_column][,1])
+      g <- g + geom_hline(yintercept=average_best, linetype="dashed", color = extra_color)
+    }
+    
+    median_column <- paste0(metric, ".median")
+    if (median_column %in% colnames(task_stats)) {
+      average_median <- mean(task_stats[median_column][,1])
+      g <- g + geom_hline(yintercept=average_median, linetype="dashed", color = extra_color)
     }
   
     plots <- append(plots, list(g))
-    #g
   }
   
-  pdf(file = "boxplots.pdf", width = 7, height = 3.5)
-  grid.arrange(grobs = plots, nrow = 1, ncol = length(metrics))
+  pdf(file = "boxplots.pdf", width = 2.33 * length(plots), height = 3.5)
+  grid.arrange(grobs = plots, nrow = 1, ncol = length(plots))
   dev.off()
 }
 boxplots(results)
@@ -120,6 +163,11 @@ topic_plots <- function(results) {
       filter(measure==metric,topic!='all') %>%
       mutate(value = as.numeric(value)) %>%
       mutate(topic = factor(topic, levels=unique(sort(as.numeric(topic))))) # Sort topics
+    
+    # Skip metric if data is not available (e.g. infNDCG for CT 2017)
+    if (nrow(results_per_topic) == 0) {
+      next
+    }
   
     g <- ggplot(data=results_per_topic, aes(x=topic, y=value, group=run)) +
       geom_line(aes(color=run, linetype=run)) +
@@ -133,15 +181,22 @@ topic_plots <- function(results) {
             legend.position = "none",
             legend.title = element_blank())
   
-    if (exists("stats")) {
-      g <- g + geom_line(data=stats,
-                         aes(x=stats[,1], y=stats[,i*3-1], group=NULL),  # Best
+    best_column <- paste0(metric, ".best")
+    if (best_column %in% colnames(task_stats)) {
+      best_column <- sym(best_column)
+      g <- g + geom_line(data=task_stats,
+                         aes(x=yeartopic, y=!!best_column, group="best"),  # Best
                          size=0.5,
-                         color=extra_color) +
-        geom_line(data=stats,
-                  aes(x=stats[,1], y=stats[,i*3], group=NULL),      # Median
-                  size=0.5,
-                  color=extra_color)
+                         color=extra_color)
+    }
+    
+    median_column <- paste0(metric, ".median")
+    if (median_column %in% colnames(task_stats)) {
+      median_column <- sym(median_column)
+      g <- g + geom_line(data=task_stats,
+                                 aes(x=yeartopic, y=!!median_column, group="median"),      # Median
+                                 size=0.5,
+                                 color=extra_color)
     }
     
     plots <- append(plots, list(g))
@@ -150,8 +205,8 @@ topic_plots <- function(results) {
   # Add legend only to the last plot to save space
   plots[length(plots)][[1]] <- plots[length(plots)][[1]] + theme(legend.position = "bottom")
   
-  pdf(file = "topics.pdf", width = 10, height = 14)
-  grid.arrange(grobs = plots, nrow = length(metrics), ncol = 1)
+  pdf(file = "topics.pdf", width = 10, height = 4.67 * length(plots))
+  grid.arrange(grobs = plots, nrow = length(plots), ncol = 1)
   dev.off()
 }
 topic_plots(results)
@@ -177,3 +232,5 @@ precision_recall <- function(results) {
   dev.off()
 }
 precision_recall(results)
+
+setwd(old_wd)
