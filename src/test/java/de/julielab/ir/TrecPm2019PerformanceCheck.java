e4fbd86b465d03402bc5d86f2d4d390b25fe3da2
khituras <chew@gmx.net>
2019-12-05 14:28:34 +0100

    Finished the `TrecPm2019PerformanceCheck` official results test class.
    There are tests to reproduce all our best-scored official results in the result table of the overview paper.
    This should help to keep track of unintended performance degradations (and also increases of scores, actually).

--- src/test/java/de/julielab/ir/TrecPm2019PerformanceCheck.java
+++ src/test/java/de/julielab/ir/TrecPm2019PerformanceCheck.java
@@ -1,9 +1,9 @@
 package de.julielab.ir;
 
 import at.medunigraz.imi.bst.config.TrecConfig;
-import at.medunigraz.imi.bst.lexigram.Lexigram;
 import at.medunigraz.imi.bst.trec.experiment.Experiment;
 import at.medunigraz.imi.bst.trec.experiment.TrecPmRetrieval;
+import at.medunigraz.imi.bst.trec.experiment.registry.ClinicalTrialsRetrievalRegistry;
 import at.medunigraz.imi.bst.trec.experiment.registry.LiteratureArticlesRetrievalRegistry;
 import at.medunigraz.imi.bst.trec.model.Metrics;
 import at.medunigraz.imi.bst.trec.model.Topic;
@@ -26,21 +26,46 @@ import static org.junit.Assert.assertEquals;
  */
 public class TrecPm2019PerformanceCheck {
 
-    private static final TrecQrelGoldStandard<Topic> GOLD_STANDARD = TrecPMGoldStandardFactory.pubmedOfficial2019();
+    private static final TrecQrelGoldStandard<Topic> PM_19_GOLD_STANDARD = TrecPMGoldStandardFactory.pubmedOfficial2019();
+    private static final TrecQrelGoldStandard<Topic> CT_19_GOLD_STANDARD = TrecPMGoldStandardFactory.trialsOfficial2019();
 
     @Before
     public void SetUp() {
         Assume.assumeTrue(ConnectionUtils.checkElasticOpenPort());
+        TrecConfig.SUBTEMPLATES_FOLDER = "/subtemplates/";
     }
 
     @Test
     public void testJlPmCommon2() {
-        TrecConfig.SUBTEMPLATES_FOLDER = "/subtemplates/";
         TrecPmRetrieval trecPmRetrieval = LiteratureArticlesRetrievalRegistry.jlpmcommon2(1000);
-        Experiment exp = new Experiment(GOLD_STANDARD, trecPmRetrieval);
+        Experiment exp = new Experiment(PM_19_GOLD_STANDARD, trecPmRetrieval);
         Metrics metrics = exp.run();
         assertEquals("jlpmcommon2 infNGDC result is wrong", 0.5783, metrics.getInfNDCG(), 0.0001);
         assertEquals("jlpmcommon2 R-prec result is wrong", 0.3572, metrics.getRPrec(), 0.0001);
         assertEquals("jlpmcommon2 P@10 result is wrong", 0.6525, metrics.getP10(), 0.0001);
+    }
+
+    @Test
+    public void testJlCtGenes() {
+        TrecPmRetrieval jlctgenes = ClinicalTrialsRetrievalRegistry.jlctgenes(1000);
+        Experiment exp = new Experiment(CT_19_GOLD_STANDARD, jlctgenes);
+        Metrics metrics = exp.run();
+        assertEquals("jlctgenes infNGDC result is wrong", 0.6451, metrics.getInfNDCG(), 0.0001);
+    }
+
+    @Test
+    public void testJlCtPrec() {
+        TrecPmRetrieval jlctprec = ClinicalTrialsRetrievalRegistry.jlctprec(1000);
+        Experiment exp = new Experiment(CT_19_GOLD_STANDARD, jlctprec);
+        Metrics metrics = exp.run();
+        assertEquals("jlctprec R-prec result is wrong", 0.4820, metrics.getRPrec(), 0.0001);
+    }
+
+    @Test
+    public void testJlCtPhrase() {
+        TrecPmRetrieval jlctphrase = ClinicalTrialsRetrievalRegistry.jlctphrase(1000);
+        Experiment exp = new Experiment(CT_19_GOLD_STANDARD, jlctphrase);
+        Metrics metrics = exp.run();
+        assertEquals("jlctphrase P@10 result is wrong", 0.5474, metrics.getP10(), 0.0001);
     }
 }
