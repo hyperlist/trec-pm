792e418a97739b617d708e30833f49bc857dfde0
khituras <chew@gmx.net>
2020-01-13 09:53:22 +0100

    Removed the time constraint from all SMAC configurations. The default was 48 hours which is too little.

--- scripts/runBaSplit.sh
+++ scripts/runBaSplit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-smac-v2.10.03-master-778/smac --validation false --scenario-file config/smac/allparams_ba_split$1/scenario.txt
+smac-v2.10.03-master-778/smac --validation false --scenario-file config/smac/allparams_ba_split$1/scenario.txt --restore-scenario smac-output/allparams_ba_split$1/state-run1 --rungroup allparams_ba_split$i
--- scripts/runCtSplit.sh
+++ scripts/runCtSplit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-smac-v2.10.03-master-778/smac --validation false --scenario-file config/smac/allparams_ct_split$1/scenario.txt
+smac-v2.10.03-master-778/smac --validation false --scenario-file config/smac/allparams_ct_split$1/scenario.txt --restore-scenario smac-output/allparams_ct_split$1/state-run1 --rungroup allparams_ct_split$i
